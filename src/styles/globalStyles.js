051741d27e51362323faf1570683e9c20fbc55d9
wook95 <uclee95@gmail.com>
2022-03-24 01:16:20 +0900

    style: reset css 추가

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+const styles = css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+    overflow-y: scroll;
+  }
+
+  body {
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    font-family: inherit;
+  }
+
+  ul,
+  ol,
+  li {
+    list-style: none;
+  }
+`;
+
+const GlobalStyles = createGlobalStyle`
+    ${styles};
+`;
+
+export default GlobalStyles;
