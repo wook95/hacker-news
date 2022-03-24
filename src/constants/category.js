60a98d9868483721fb7ef760de0b3e1f06e60815
wook95 <uclee95@gmail.com>
2022-03-25 00:33:10 +0900

    UI: index페이지 ui 구현

---
+++ src/constants/category.js
@@ -0,0 +1,29 @@
+const CATEGORIES = [
+  {
+    id: 1,
+    name: 'TOP',
+    endPoint: 'topstories',
+  },
+  {
+    id: 2,
+    name: 'NEW',
+    endPoint: 'newstories',
+  },
+  {
+    id: 3,
+    name: 'ASK',
+    endPoint: 'askstories',
+  },
+  {
+    id: 4,
+    name: 'SHOW',
+    endPoint: 'showstories',
+  },
+  {
+    id: 5,
+    name: 'JOB',
+    endPoint: 'jobstories',
+  },
+];
+
+export default CATEGORIES;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,52 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import CATEGORIES from '@/constants/category';
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Container>
+      <Title>HACKER NEWS</Title>
+      <Lists>
+        {CATEGORIES.map(list => {
+          return (
+            <List>
+              <Link to={`/list/${list.endPoint}`}>{list.name}</Link>
+            </List>
+          );
+        })}
+      </Lists>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  ${props => props.theme.wrapper}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  font-size: 1.6rem;
+`;
+
+const Title = styled.h1`
+  margin: 10rem 0 5rem;
+  font-weight: 200;
+`;
+
+const Lists = styled.ul`
+  display: flex;
+  gap: 1rem;
+`;
+
+const List = styled.li`
+  padding: 0.5rem 1rem;
+  border-radius: 0.3rem;
+  font-size: 2.5rem;
+  font-weight: 600;
+
+  &:hover {
+    color: ${props => props.theme.colors.gray500};
+  }
+`;
 
 export default Home;
