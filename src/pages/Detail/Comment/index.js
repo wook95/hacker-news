0c4804a597cc883eb5271153a2e82ce0ca18e7b1
wook95 <uclee95@gmail.com>
2022-03-27 21:10:17 +0900

    UI: 상세페이지 UI 구현

--- src/pages/Detail/Comment/index.js
+++ src/pages/Detail/Comment/index.js
@@ -1,4 +1,6 @@
 import { useQueries } from 'react-query';
+import styled from 'styled-components';
+import DOMPurify from 'dompurify';
 import LoadingSpinner from '@/components/base/LoadingSpinner';
 import getDetail from '@/apis/api/detail';
 import calculateTimeAgo from '@/utils/function/calculateTimeBefore';
@@ -12,20 +14,56 @@ const Comment = ({ comments }) => {
   );
 
   return (
-    <div>
-      {commentQueries.map(res =>
+    <Container>
+      <Title>{comments.length} Comments</Title>
+      {commentQueries.map((res, index) =>
         res.isLoading ? (
-          <LoadingSpinner />
+          <LoadingSpinner key={index} />
         ) : (
-          <div key={res.data.id}>
-            <p>{res.data.by}</p>
-            <p>{res.data.text}</p>
-            <p>{calculateTimeAgo(res.data.time)}</p>
-          </div>
+          <CommentCard key={res.data.id}>
+            <Author>{res.data.by}</Author>
+            <Content
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(res.data.text),
+              }}
+            />
+            <Status>{calculateTimeAgo(res.data.time)}</Status>
+          </CommentCard>
         ),
       )}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  margin-top: 2rem;
+  padding: 3rem;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 1rem;
+  font-size: 1.6rem;
+`;
+
+const CommentCard = styled.div`
+  min-height: 5rem;
+  padding: 2rem 0;
+  border-bottom: 1px solid #ccc;
+  font-size: 1.4rem;
+`;
+
+const Author = styled.p`
+  margin-bottom: 0.3rem;
+  color: ${props => props.theme.colors.primary};
+`;
+
+const Content = styled.p`
+  margin: 1rem 0;
+  line-height: 1.5;
+`;
+
+const Status = styled.p`
+  color: ${props => props.theme.colors.gray100};
+`;
 
 export default Comment;
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,9 +1,9 @@
 import { useParams } from 'react-router-dom';
-import { useQuery, useQueries } from 'react-query';
+import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import DOMPurify from 'dompurify';
-import Comment from '@/pages/Detail/Comment';
 import LoadingSpinner from '@/components/base/LoadingSpinner';
+import Comment from '@/pages/Detail/Comment';
 import getDetail from '@/apis/api/detail';
 import calculateTimeAgo from '@/utils/function/calculateTimeBefore';
 
@@ -25,26 +25,67 @@ const Detail = () => {
       {isLoading ? (
         <LoadingSpinner />
       ) : (
-        <div>
-          <p>title: {postDetail.title}</p>
-          <p>author: {postDetail.by}</p>
-          <p>time: {calculateTimeAgo(postDetail.time)}</p>
-          <p>score: {postDetail.score}</p>
-          {postDetail?.text && (
-            <>
-              <span>text </span>
-              <span
+        <>
+          <DetailCard>
+            <Title>{postDetail.title}</Title>
+            <Author>{postDetail.by}</Author>
+            <StatusBar>
+              <p>{calculateTimeAgo(postDetail.time)}</p>
+              <p>{postDetail.score} score </p>
+              {postDetail?.url && (
+                <ExternalLink href={postDetail.url} target='_blank'>
+                  Link
+                </ExternalLink>
+              )}
+            </StatusBar>
+            {postDetail?.text && (
+              <Content
                 dangerouslySetInnerHTML={{
                   __html: DOMPurify.sanitize(postDetail.text),
-                }}></span>
-            </>
-          )}
-          {postDetail?.url && <p>url {postDetail.url}</p>}
+                }}
+              />
+            )}
+          </DetailCard>
           {comments && <Comment comments={comments} />}
-        </div>
+        </>
       )}
     </>
   );
 };
+
+const DetailCard = styled.div`
+  padding: 3rem 3rem 5rem 3rem;
+  margin-bottom: 1rem;
+  font-size: 1.4rem;
+  border: 0.1rem solid ${props => props.theme.colors.gray800};
+  box-shadow: 0.2rem 0.2rem 0.2rem 0.1rem ${props => props.theme.colors.gray800};
+`;
+
+const Title = styled.h3`
+  margin-bottom: 0.7rem;
+  font-size: 1.9rem;
+`;
+
+const Author = styled.p`
+  margin-bottom: 0.6rem;
+  color: ${props => props.theme.colors.primary};
+`;
+
+const StatusBar = styled.div`
+  padding-bottom: 1rem;
+  margin-bottom: 3rem;
+  display: flex;
+  gap: 1.5rem;
+  border-bottom: 1px solid #ccc;
+  color: ${props => props.theme.colors.gray100};
+`;
+
+const ExternalLink = styled.a`
+  color: ${props => props.theme.colors.blue};
+`;
+
+const Content = styled.div`
+  line-height: 1.8;
+`;
 
 export default Detail;
